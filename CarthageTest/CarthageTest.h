04d191b91cb6ad00802ffb43ec68bceb91617acf
Vladimir Psyukalov <vpsyukalov@zuzex.com>
2019-12-30 10:31:49 +0300

    First init

---
+++ CarthageTest/CarthageTest.h
@@ -0,0 +1,19 @@
+//
+//  CarthageTest.h
+//  CarthageTest
+//
+//  Created by Vladimir Psyukalov on 30.12.2019.
+//  Copyright © 2019 ZUZEX LLC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CarthageTest.
+FOUNDATION_EXPORT double CarthageTestVersionNumber;
+
+//! Project version string for CarthageTest.
+FOUNDATION_EXPORT const unsigned char CarthageTestVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CarthageTest/PublicHeader.h>
+
+
---
+++ CarthageTest/CarthageTest.swift
@@ -0,0 +1,15 @@
+//
+//  CarthageTest.swift
+//  CarthageTest
+//
+//  Created by Vladimir Psyukalov on 30.12.2019.
+//  Copyright © 2019 ZUZEX LLC. All rights reserved.
+//
+
+import Foundation
+
+public class MyTestClass {
+    public func printSomething(_ something: String? = nil) {
+        print(something ?? "Empty string")
+    }
+}
